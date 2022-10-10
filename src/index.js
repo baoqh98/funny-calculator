7b51b91d9e2ccb33c6d70837a313136fc22be8bb
Quoc Bao <hoquocbao@protonmail.com>
2022-10-10 15:50:48 +0700

    remove redux

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import { Provider } from 'react-redux';
-import store from './store/store';
+
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 
@@ -10,8 +9,6 @@ const root = createRoot(container);
 
 root.render(
   <BrowserRouter>
-    <Provider store={store}>
-      <App />
-    </Provider>
+    <App />
   </BrowserRouter>
 );
--- src/store/store.js
+++
@@ -1,8 +0,0 @@
-import { configureStore } from '@reduxjs/toolkit';
-import calculatorReducer from '../Modules/Calculator/calculatorSlice';
-
-export default configureStore({
-  reducer: {
-    calc: calculatorReducer,
-  },
-});
